0b99e3874cafdca41463a7e953976a734edbde31
João Freire <joao.campos105115@gmail.com>
2025-09-14 12:42:02 -0300

    Tarefa 4: geração de imagem com vec e color

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 
 int main() {
@@ -13,20 +16,16 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
             int ix = i / square_size;
             int jy = j / square_size;
 
             bool is_white = ((ix + jy) % 2 == 0);
 
-            int r, g, b;
-            if (is_white) {
-                r = g = b = 255;  // branco
-            } else {
-                r = g = b = 0;    // preto
-            }
-
-            std::cout << r << " " << g << " " << b << "\n";
+            color pixel_color = is_white ? color(1,1,1) : color(0,0,0);
+            write_color(std::cout, pixel_color);
         }
     }
+    std::clog << "\rDone.                 \n";
 }
