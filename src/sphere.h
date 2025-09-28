7c81d3c6ffd3355a71573ad411d4d5bc41ad1a04
João Freire <joao.campos105115@gmail.com>
2025-09-28 13:06:08 -0300

    Tarefa 16: Classe Material

--- src/sphere.h
+++ src/sphere.h
@@ -5,8 +5,9 @@
 
 class sphere : public hittable {
 public:
-    sphere(const point3& center, double radius) : center(center), radius(std::fmax(0,radius)) {}
-
+    sphere(const point3& center, double radius) : center(center), radius(std::fmax(0,radius)) {
+        // TODO: Initialize the material pointer `mat`.
+    }
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {        vec3 oc = center - r.origin();
         auto a = r.direction().length_squared();
         auto h = dot(r.direction(), oc);
@@ -30,6 +31,7 @@ public:
         rec.p = r.at(rec.t);
         vec3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
+        rec.mat = mat;
 
         return true;
     }
@@ -37,6 +39,7 @@ public:
 private:
     point3 center;
     double radius;
+    shared_ptr<material> mat;
 };
 
 #endif
